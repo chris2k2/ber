04645c37da653ad02de70e55e10b44063cd4cb6a
chris <github@cweyermann.de>
2018-07-09 19:15:21 +0200

    missed files

---
+++ tournaments/src/main/java/de/cweyermann/ber/tournaments/boundary/sqs/QueueError.java
@@ -0,0 +1,15 @@
+package de.cweyermann.ber.tournaments.boundary.sqs;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class QueueError
+{
+    String stacktrace;
+    
+    String reason;
+    
+    Date time;
+}
